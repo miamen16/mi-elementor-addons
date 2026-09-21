8de0d4045807b5f5d7a2cd93d3b7f93b36bc660a
Mohamed Ibrahim Ibrahim <31406965+miamen16@users.noreply.github.com>
2026-09-21 14:04:09 +0300

    chore: initialize MI Elementor Addons with Product Card

---
+++ uninstall.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Uninstall MI Elementor Addons.
+ *
+ * @package MI_Elementor_Addons
+ */
+
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
+
+// No persistent data is currently stored by the plugin.
